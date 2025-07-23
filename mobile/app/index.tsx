816af4e80a9e41fdc6f698685bbfe4e01623f11b
MikiZenebe <mickyaszenebe@gmail.com>
2025-07-23 17:34:21 +0300

    fix

--- mobile/app/index.tsx
+++ mobile/app/index.tsx
@@ -4,7 +4,7 @@ export default function Index() {
   return (
     <View className="bg-black flex flex-col justify-center  h-[100vh] w-full">
       <Text className="font-bold text-xl text-white text-center">
-        Edit app/index.tsx to edit this screen.
+        Edit app/index.tsx to edit this screen
       </Text>
     </View>
   );
