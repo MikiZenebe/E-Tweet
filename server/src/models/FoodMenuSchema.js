0940a74f1c8492e9b52057870457cfe453bfafa8
MikiZenebe <mickyaszenebe@gmail.com>
2025-07-23 21:25:34 +0300

    feat: setup schemas

---
+++ server/src/models/FoodMenuSchema.js
@@ -0,0 +1,26 @@
+import mongoose, { model, Schema } from "mongoose";
+
+const FoodMenuSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+    min: 0,
+  },
+  imageUrl: {
+    type: String,
+    required: true,
+  },
+});
+
+const Food = model("Food", FoodMenuSchema);
+
+export default Food;
