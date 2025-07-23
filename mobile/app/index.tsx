87110d1c7a352882cf1f2a8a1629b537a43de343
MikiZenebe <mickyaszenebe@gmail.com>
2025-07-23 17:20:14 +0300

    feat: initial

---
+++ mobile/app/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="bg-black flex flex-col justify-center  h-[100vh] w-full">
+      <Text className="font-bold text-xl text-white text-center">
+        Edit app/index.tsx to edit this screen.
+      </Text>
+    </View>
+  );
+}
