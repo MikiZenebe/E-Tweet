4c4ac4c377cfe4aa653851cb35aa32a435c50d7c
MikiZenebe <mickyaszenebe@gmail.com>
2025-08-03 00:15:32 +0300

    update

--- server/src/routes/OrderRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { createOrder, getOrder } from "../controllers/OrderController";
-
-const router = express.Router();
-
-router.post("/create/order", createOrder);
-router.post("/orders", getOrder);
-
-export default router;
