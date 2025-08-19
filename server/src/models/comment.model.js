50045b7862085d85be3fbdbbf17c153a7861edf9
MikiZenebe <mickyaszenebe@gmail.com>
2025-08-19 04:34:21 -0700

    setup-models

---
+++ server/src/models/comment.model.js
@@ -0,0 +1,32 @@
+import { Schema, model } from "mongoose";
+
+const commentSchema = new Schema(
+  {
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    post: {
+      type: Schema.Types.ObjectId,
+      ref: "Post",
+      required: true,
+    },
+    content: {
+      type: String,
+      required: true,
+      maxLength: 280,
+    },
+    likes: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+const Comment = model("Comment", commentSchema);
+
+export default Comment;
---
+++ server/src/models/notification.odel.js
@@ -0,0 +1,36 @@
+import { Schema, model } from "mongoose";
+
+const notificationSchema = new Schema(
+  {
+    from: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    to: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    type: {
+      type: String,
+      required: true,
+      enum: ["follow", "like", "comment"],
+    },
+    post: {
+      type: Schema.Types.ObjectId,
+      ref: "Post",
+      default: null,
+    },
+    comment: {
+      type: Schema.Types.ObjectId,
+      ref: "Comment",
+      default: null,
+    },
+  },
+  { timestamps: true }
+);
+
+const Notification = model("Notification", notificationSchema);
+
+export default Notification;
