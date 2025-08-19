56c24d83970aa86b55de335bdf1a2cf214bb4a8f
MikiZenebe <mickyaszenebe@gmail.com>
2025-08-19 03:10:09 -0700

    set-up env

---
+++ server/src/server.js
@@ -0,0 +1,11 @@
+import express from "express";
+import { ENV } from "./config/env.js";
+import { connectDB } from "./config/db.js";
+
+const app = express();
+
+connectDB();
+
+app.listen(ENV.PORT, () =>
+  console.log("Server is up and running on port", ENV.PORT)
+);
