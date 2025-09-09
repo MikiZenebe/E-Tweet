6fcc6adb9f8645cc36142d49b1d0921ee9e6d62a
MikiZenebe <mickyaszenebe@gmail.com>
2025-09-09 02:20:18 -0700

    message

--- mobile/app/(tabs)/search.tsx
+++ mobile/app/(tabs)/search.tsx
@@ -1,5 +1,11 @@
 import { Feather } from "@expo/vector-icons";
-import { View, TextInput, ScrollView, Text, TouchableOpacity } from "react-native";
+import {
+  View,
+  TextInput,
+  ScrollView,
+  Text,
+  TouchableOpacity,
+} from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 const TRENDING_TOPICS = [
@@ -27,12 +33,29 @@ const SearchScreen = () => {
 
       <ScrollView className="flex-1">
         <View className="p-4">
-          <Text className="text-xl font-bold text-gray-900 mb-4">Trending for you</Text>
+          <Text className="text-xl font-bold text-gray-900 mb-4">
+            Trending for you
+          </Text>
           {TRENDING_TOPICS.map((item, index) => (
-            <TouchableOpacity key={index} className="py-3 border-b border-gray-100 flex-row items-center justify-between">
-            <View>  <Text className="text-gray-500 text-sm">Trending in Technology</Text>
-              <Text className="font-bold text-gray-900 text-lg">{item.topic}</Text></View>
-             <View> <Text className="text-gray-500 text-sm">{item.tweets} Tweets</Text></View>
+            <TouchableOpacity
+              key={index}
+              className="py-3 border-b border-gray-100 flex-row items-center justify-between"
+            >
+              <View>
+                {" "}
+                <Text className="text-gray-500 text-sm">
+                  Trending in Technology
+                </Text>
+                <Text className="font-bold text-gray-900 text-lg">
+                  {item.topic}
+                </Text>
+              </View>
+              <View>
+                {" "}
+                <Text className="text-gray-500 text-sm">
+                  {item.tweets} Tweets
+                </Text>
+              </View>
             </TouchableOpacity>
           ))}
         </View>
